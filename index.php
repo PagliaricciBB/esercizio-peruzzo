0695b184d7844bef3bde6b20de3f0c0710900de3
dani128 <michielin.daniel@gmail.com>
2019-03-27 12:16:58 +0100

    ciao

---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <style type="text/css">
+      
+    </style>
+
+  <title>Fortnite</title>
+  
+  <body>
+    <h1 class="d-block p-2 bg-primary text-white">Fortnite</h1>
+  </body>
+     
+  </head>
+
+   <div class="container">
+    <div class="row">
+      </div>
+      <p>
+   <p>    
+    
+<div class="row">
+ <div class="col-md-sm">
+<h2> Battaglia reale </h2>
+  </div>
+
+        </div class="col-md-sm">
+   <img src="img/Battaglia Reale.jpg" class="img-fluid" alt="reale"> 
+</div>
+
+
+<div class="col-sm-12 col-md-8">
+ <p>La <b>Battaglia Reale</b> è ambientata su un'isola in cui 100 giocatori lottano per la sopravvivenza. La modalità è stata lanciata ufficialmente dalla Epic Games il 26 settembre 2017, mentre il trailer è stato pubblicato su Youtube il 20 settembre.[1] Il gioco presenta diverse modalità: singolo, in coppia o in squadra da tre o quattro giocatori, mentre sono presenti anche modalità a tempo limitato (LTM). Tra queste le più famose sono: Parcogiochi (PlayGround), Il guanto dell'Infinito, 50vs50 o 20vs20.
+<br>
+<br>
+ Il 3 luglio 2018 è stata lanciata una modalità che prende il nome di Parco Giochi o (Playground) dove uno o più giocatori, fino a un massimo di 4, hanno a disposizione l'intera mappa per un totale di 55 minuti dove possono testare le proprie abilità e mettere alla prova la loro fantasia attraverso imponenti costruzioni e combattimenti. Essa si è rivelata la modalità a tempo limitato (LTM) più giocata.
+
+<br>
+<br>
+
+La partita ha inizio dal Vinder Bus, ovvero un bus che percorre una linea retta sorvolando l'area di gioco. I giocatori scelgono il momento in cui paracadutarsi e fluttuano sino all'area preferita dove ha inizio una corsa agli armamenti per guadagnarsi armi ed equipaggiamenti vari. Oltre agli avversari, i giocatori devono tener conto della tempesta, che lentamente si restringe in una parte casuale della mappa sempre più piccola arrecando danni a tutti coloro che restano al suo esterno. La partita ha termine quando a restare in vita è un solo giocatore o nelle competizioni in team almeno un componente di una squadra.
+
+<br>
+<br>
+
+Questa modalità è stata pubblicata, oltre che sulle piattaforme elencate sopra, anche su Nintendo Switch e iOS. Per Android è invece disponibile in versione beta per un numero limitato di dispositivi, a partire dai top di gamma Samsung,[2] esclusivamente sul sito del produttore.[3]</p>
+       
+ 
+     <div>
+      <div class="col-sm">
+        <h2> Salva il mondo </h2>
+      </div>
+    </div>
+
+      <div class="col-md-sm">
+    <img src="img/salva il mondo.jpg" alt="salva">
+  </div>
+    
+    <p>Salva <b> il mondo </b> La modalità salva il mondo è ambientata in una terra post-apocalittica, dove l'improvvisa apparizione di una tempesta mondiale ha fatto scomparire il 98% della popolazione, in parte sostituita da pericolose creature aliene. Fortnite offre modalità di cooperazione fino a quattro giocatori per il compimento di varie missioni per raccogliere risorse, costruire fortificazioni attorno a obiettivi che hanno lo scopo di aiutare a combattere la tempesta e proteggere i sopravvissuti, e costruire armi e trappole per affrontare le ondate di queste creature. I giocatori ottengono ricompense attraverso queste missioni per migliorare le loro abilità. Il gioco è caratterizzato inoltre da microtransazioni per acquistare valuta di gioco (V-bucks), che può essere utilizzata per ottenere aggiornamenti. Questa modalità è stata pubblicata su Microsoft Windows, macOS, PlayStation 4 e Xbox One.</p>
+     
+    <div>
+      <div class="col-md-sm">
+   <h2> Creativa </h2>
+       
+        <div class="col-md-sm">
+      <img src="img/Modalita creativa.jpg" alt="creativa" align="left" width="200" height="150">
+    </div>
+    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <p> La <b> modalità creativa</b> Con Creativa, potremo personalizzare una mappa in tutti i suoi dettagli. Dalle opzioni del terreno, dello scenario, sino alla presenza degli edifici, dei tesori e delle armi. Si può scegliere se creare una mappa personalizzata e giocarla solo con gli amici oppure condividerla e aspettare che altri gamer si uniscano al gioco. Si possono anche pubblicare le mappe in una speciale community realizzata da Epic Games per creare delle vere e proprie campagne con mappe diverse e con scenari e armi sempre diversi. Si tratta fondamentalmente di un aggiornamento, con importanti migliorie, della già presente modalità Fortnite Playground LTM.</p>
+    
+
+
+<?php
+  $saluta = true;
+  if ($saluta == true) {
+    print "ciao";
+  } else {
+    print "no";
+  }
+
+?>
+
+  
+  <footer>
+    <div class="container">
+      <hr>
+      <footer><em>Tratto da Wikipedia: Fortnite - Wikipedia.
+      Jonathan Pagliaricci 2'A</em></footer>
+    </div>
+  </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html> 
